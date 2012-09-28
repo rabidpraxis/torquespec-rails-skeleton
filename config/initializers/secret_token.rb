079e9e9969fef884e21c401d4d1c1db33e91ed95
rabidpraxis <webster.kevin@gmail.com>
2012-09-28 10:10:39 -0700

    rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TorquespecRailsSkeleton::Application.config.secret_token = '466d91eec74ae51fcec4eceefbf2b2f9a220ddfad86affef45b032bfd8a979ebab469f34cb9776efff6c809252771c3e0450dea714a50638dca0b657bca96ded'
